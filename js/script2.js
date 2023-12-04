398bf84399c14e03d21cff4a5df88534c4420d20
K3NTd02 <146251438+K3NTd02@users.noreply.github.com>
2023-12-04 15:09:29 -0800

    Add files via upload

--- js/script2.js
+++ js/script2.js
@@ -23,6 +23,70 @@ function createMap(){
     getData();
 };
 
+function pointToLayer(feature, latlng){
+    //create marker options
+    var options;
+    if (feature.properties.PrimSource == "solar") {
+        options = {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+    } else if (feature.properties.PrimSource == "hydroelectric") {
+        options = {
+            radius: 8,
+            fillColor: "#0504aa",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+    } else if (feature.properties.PrimSource == "wind") {
+        options = {
+            radius: 8,
+            fillColor: "#04d9ff",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+    } else {
+        options = {
+            radius: 8,
+            fillColor: "#0000ff",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+    }
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "";
+    popupContent += "<p><b>Plant Name:</b> " + feature.properties.Plant_Name + "</p>";
+    popupContent += "<p><b>City:</b> " + feature.properties.City + "</p>";
+    popupContent += "<p><b>Total Megawatts Produced:</b> " + feature.properties.Total_MW + " MW</p>";
+    popupContent += "<p><b>Main Energy Source:</b> " + feature.properties.PrimSource + "</p>";
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createSymbols(data, map){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(map);
+};
+
 function onEachFeature(feature, layer) {
     //no property named popupContent; instead, create html string with all properties
     var popupContent = "";
@@ -36,7 +100,7 @@ function onEachFeature(feature, layer) {
 //function to retrieve the data and place it on the map
 function getData(){
     //load the data
-    fetch("/Dana_Final_Project/data/Power_Plants.geojson")
+    fetch("/data/Power_Plants.geojson")
         .then(function(response){
             return response.json();
         })
